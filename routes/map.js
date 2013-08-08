20b4be37055f4f57ab119539feaa35d8ec8e3bc0
tmorozov <radist@desant.biz>
2013-08-08 12:11:07 +0300

    correct center

--- routes/map.js
+++ routes/map.js
@@ -8,7 +8,7 @@ var db = require('../db');
 exports.show = function(req, res){
   db.connection().query('SELECT * FROM locations;', function (error, result) {
     // Rjasne by default
-    var center = req.param('position') ? req.param('position') : '49.861227,23.923500';
+    var center = req.param('position') ? req.param('position') : '49.86240,23.92150';
     var markers = "";
     result.rows.forEach(function (item) {
       markers += "&markers=icon:http://team-radar.herokuapp.com/images/target.png%7Clabel:S%7C"+item.lat+","+item.lon;
