4d8a28cff2a5368d49d8d06713fc3beb12739b75
Timur Morozov <radist@desant.biz>
2013-08-10 21:10:02 +0300

    use data separately

--- routes/goal.js
+++ routes/goal.js
@@ -1,7 +1,29 @@
+var data = require('../data');
+
 exports.index = function(req, res){
-  res.render('goal', { title: 'Current Goals' });
+  var goals = {};
+  Object.keys(data.targets).forEach(function(key) {
+    var val = data.targets[key];
+    goals[key] = val;
+  });
+
+  res.render('goal', { 
+    title: 'Current Goals',
+    goals: goals
+  });
 };
 
 exports.create = function(req, res){
-  res.render('goal', { title: 'Current Goals' });
+  var goals = {};
+  Object.keys(data.targets).forEach(function(key) {
+    var val = data.targets[key];
+    goals[key] = val;
+  });
+
+  goals[req.param('goal')] = [0, 0];
+
+  res.render('goal', { 
+    title: 'Current Goals',
+    goals: goals
+  });
 };
--- routes/map.js
+++ routes/map.js
@@ -1,27 +1,16 @@
-var npcs = {
-  npc1 : [49.8623, 23.9172],
-  npc2 : [49.8633, 23.9182],
-  npc3 : [49.8613, 23.9162]
-};
-
-var targets = {
-  'code1' : [49.8622, 23.9171],
-  'code2' : [49.8624, 23.9173],
-  'code3' : [49.8622, 23.9171],
-  'code4' : [49.8621, 23.9174]
-};
+var data = require('../data');
 
 function addPoints(points, src, label) {
   Object.keys(src).forEach(function(key) {
     var val = src[key];
     points.push([key, val[0], val[1], label]);
-  });    
+  });
 }
 
 exports.dynamic = function(req, res) {
     var points = [];
-    addPoints(points, npcs, 'npc');
-    addPoints(points, targets, 'target');
+    addPoints(points, data.npcs, 'npc');
+    addPoints(points, data.targets, 'target');
 
     var centerStr = req.param('position') ? req.param('position') : '49.86240,23.92150';
     var center = centerStr.split(',');
