c186fd7e2bd77c01e3508819f777b2f96f810083
Timur Morozov <radist@desant.biz>
2013-09-22 00:16:42 +0300

    show me

--- public/javascripts/ngapp/app.js
+++ public/javascripts/ngapp/app.js
@@ -36,7 +36,7 @@ function ctrQuests($scope) {
   $scope.location = 'Quests';
 }
 
-function ctrMap($scope, $log) {
+function ctrMap($scope) {
   $scope.location = 'Map';
 
   function initializeMap() {
@@ -52,26 +52,86 @@ function ctrMap($scope, $log) {
     return map;
   }
 
-  function showOverlay(map, markers) {
-    var points = markers || [];
-    var image = '/images/target.png';
-    var myLatLng = new google.maps.LatLng(49.8622, 23.9171);
+  function showOverlay(map, points) {
+    if(! points) {
+      return [];
+    }
+
+    var markers = [];
 
     for(var i=points.length-1; i>=0; i--) {
       var point = points[i];
-      var myLatLng = new google.maps.LatLng(point[1], point[2]);
-      var beachMarker = new google.maps.Marker({
-        position: myLatLng,
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(point[1], point[2]),
         map: map,
         title: point[0],
         icon: '/images/'+point[3]+'.png'
       });
+
+      markers.push(marker);
     }
 
-    return map;
+    return markers;
   }
 
-  $scope.map = initializeMap();
-  $scope.makrers = showOverlay($scope.map, null);
+  function removeMarker(marker) {
+    if(!marker) {
+      return;
+    }
+    marker.setMap(null);
+  }
 
+  function displayPosition(position) {
+    displayPosition.counter++;
+    var points = [
+      ["Я", position.coords.latitude, position.coords.longitude, "me"]
+    ];
+
+    removeMarker($scope.myPosition);
+    $scope.myPosition = showOverlay($scope.map, points);
+
+    console.log("Lat: " + position.coords.latitude + 
+      ", Lon: " + position.coords.longitude +
+      ", accuracy:" + position.coords.accuracy +
+      ", counter:" + displayPosition.counter +
+      ", timestamp: " + new Date(position.timestamp));
+  }
+
+  displayPosition['counter'] = 0;
+
+  function displayError(error) {
+    var errors = { 
+      1: 'Permission denied',
+      2: 'Position unavailable',
+      3: 'Request timeout'
+    };
+    console.log("Error: " + errors[error.code]);
+  }
+
+  function activateMyPosition() {
+    if (navigator.geolocation) {
+      var watchId = navigator.geolocation.watchPosition(
+        displayPosition, displayError, { 
+          enableHighAccuracy: true, 
+          maximumAge: 60000, // 1 min
+          timeout: 30000 // 30 sec
+        }
+      );
+    } else {
+      return false;
+    }
+  }
+
+  var points = [
+    ["npc1",49.862155,23.916207,"npc"],
+    ["npc2",49.864416,23.921962,"npc"],
+    ["npc3",49.860313,23.925817,"npc"],
+    ["2b2",49.86498,23.916439,"target"],
+    ["6i4",49.863714,23.919054,"target"]
+  ];
+
+  $scope.map = initializeMap();
+  $scope.markers = showOverlay($scope.map, points);
+  $scope.myPosition = false;
+  activateMyPosition();
 }
